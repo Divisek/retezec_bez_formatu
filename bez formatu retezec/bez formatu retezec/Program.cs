9e3a3edd256485ee8ef64da3591473e51331e944
Divisek <113785426+Divisek@users.noreply.github.com>
2023-01-22 18:57:15 +0100

    Add files via upload

---
+++ bez formatu retezec/bez formatu retezec/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 10;
+int b = 20;
+int c = 30;
+string retezec = string.Format("Když sečteme {0} a {1}, dostaneme {2}", a, b, c);
+Console.WriteLine(retezec);
+Console.ReadKey();
